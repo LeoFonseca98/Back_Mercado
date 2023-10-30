f926da06801f7ba52ea283bfdca9e89aad02fe03
higooliver7 <88789809+higooliver7@users.noreply.github.com>
2023-10-30 13:54:58 -0300

    Update produto.py

--- produto.py
+++ produto.py
@@ -1,12 +1,23 @@
 from peewee import Model, CharField, PostgresqlDatabase
 from config import Config
 
-db = PostgresqlDatabase(Config.DATABASE_URI())
+# Configurar o banco de dados usando as informações do arquivo config.py
+db = PostgresqlDatabase(Config.DATABASE['name'],
+                        user=Config.DATABASE['user'],
+                        password=Config.DATABASE['password'],
+                        host=Config.DATABASE['host'],
+                        port = Config.DATABASE.get('port', 5432))
 
-class BaseModel(Model):
-    class Meta:
-        database = db
-class Produto(BaseModel):
+# Definir o modelo de Produto usando Peewee
+class Produto(Model):
     nome = CharField(max_length=100)
     descricao = CharField(max_length=100)
+
+    class Meta:
+        database = db
+        table_name = 'produtos'  
+
+db.connect()
+db.create_tables([Produto])
+db.close()
     
